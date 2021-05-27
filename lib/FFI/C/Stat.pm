821339ad8634bfeafb199f5b09432739f63974d4
Graham Ollis <plicease@cpan.org>
2021-05-27 11:09:52 -0600

    starter very partial implementation

--- lib/FFI/C/Stat.pm
+++ lib/FFI/C/Stat.pm
@@ -3,9 +3,71 @@ package FFI::C::Stat;
 use strict;
 use warnings;
 use 5.008004;
+use Ref::Util qw( is_ref is_globref);
+use Carp ();
+use FFI::Platypus 1.00;
 
 # ABSTRACT: Object-oriented FFI interface to native stat and lstat
 # VERSION
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+my $ffi = FFI::Platypus->new( api => 1 );
+$ffi->bundle;      # for accessors / constructor / destructor
+
+$ffi->type('object(FFI::C::Stat)', 'stat');
+
+$ffi->mangler(sub { "stat__$_[0]" });
+
+$ffi->attach( '_stat'  => ['string'] => 'opaque' );
+$ffi->attach( '_fstat' => ['int',  ] => 'opaque' );
+$ffi->attach( '_lstat' => ['string'] => 'opaque' ) unless $^O eq 'MSWin32';
+
+
+=head1 CONSTRUCTOR
+
+=head2 new
+
+ my $stat = FFI::C::Stat->new(*HANDLE);
+ my $stat = FFI::C::Stat->new($filename);
+
+You can create a new instance of this class by calling the new method and passing in
+either a file or directory handle, or by passing in the filename path.
+
+=cut
+
+sub new
+{
+  my($class, $file, %options) = @_;
+
+  my $ptr;
+  if(is_globref $file)
+  { $ptr = _fstat(fileno($file)) }
+  elsif(!is_ref($file) && defined $file)
+  { $ptr = _stat($file) }
+  else
+  { Carp::croak("Tried to stat something whch is neither a glob reference nor a plain string") }
+
+  bless \$ptr, $class;
+}
+
+$ffi->attach( DESTROY => ['stat'] );
+
+=head1 PROPERTIES
+
+=head2 size
+
+ my $size = $stat->size;
+
+Returns the size of the file in bytes.
+
+=cut
+
+$ffi->attach( size => ['stat'] => 'off_t' );
 
 1;
 
--- t/ffi_c_stat.t
+++ t/ffi_c_stat.t
@@ -1,8 +1,32 @@
 use Test2::V0 -no_srand => 1;
 use FFI::C::Stat;
 
-ok 1, 'todo';
+is(
+  FFI::C::Stat->new('corpus/xx.txt'),
+  object {
+    call [ isa => 'FFI::C::Stat' ] => T();
+    call size => 3;
+  },
+  'do a stat on a regular file',
+);
+
+
+{
+  my $fh;
+  open $fh, '<', 'corpus/xx.txt';
+  is(
+    FFI::C::Stat->new($fh),
+    object {
+      call [ isa => 'FFI::C::Stat' ] => T();
+      call size => 3;
+    },
+    'do a stat on a filehandle',
+  );
+
+  close $fh;
+}
+
+
+
 
 done_testing;
-
-
